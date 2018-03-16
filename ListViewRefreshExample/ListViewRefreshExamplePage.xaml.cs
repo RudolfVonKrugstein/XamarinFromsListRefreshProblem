f5353059d03f76c07df4ed49543ce575bad964a0
Nathan Hüsken <finance@wintercloud.de>
2018-03-16 15:54:05 +0100

    Add Example project

---
+++ ListViewRefreshExample/ListViewRefreshExamplePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ListViewRefreshExample
+{
+    public partial class ListViewRefreshExamplePage : ContentPage
+    {
+        public ListViewRefreshExamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
