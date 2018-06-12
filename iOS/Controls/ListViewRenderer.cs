eee143b150ccad93ddb499f23e135759853b778b
Nathan Hüsken <nathan@wintercloud.de>
2018-06-12 15:10:48 +0200

    Add custom renderer on iOS

---
+++ iOS/Controls/ListViewRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ListView), typeof(ListViewRefreshExample.iOS.Controls.ListViewRenderer))]
+namespace ListViewRefreshExample.iOS.Controls
+{
+    class ListViewRenderer : Xamarin.Forms.Platform.iOS.ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+            if (this.Control != null)
+            {
+                this.Control.IsAccessibilityElement = false;
+            }
+        }
+    }
+}
